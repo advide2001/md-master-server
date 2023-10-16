f9e6fa59df5be54512f9a312a14f47f7cfbc2a24
Adarsh Desai <desaiadarsh22@gmail.com>
2023-10-16 13:37:57 +0530

    feat: Install and configure TypeScript, ESLint, Prettier and Husky

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-async function main() {
-  console.log("Starting prisma...");
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+console.log('Hello');
