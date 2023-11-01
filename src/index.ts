1b6f01414cb05a01d052b605d5e5979349a34e7b
Adarsh Desai <desaiadarsh22@gmail.com>
2023-11-01 14:03:02 +0530

    refactor: Update 'index.ts' to start the server with configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello');
+import { port } from './config/environment';
+import app from './app';
+
+const start = async () => {
+  try {
+    app.listen(port);
+    console.log(`server running on port ${port}...`);
+  } catch {
+    console.log('server not failed to start!');
+  }
+};
+
+start();
