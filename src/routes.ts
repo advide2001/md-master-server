9b9dfeec344e1d54b0a5a0b4252d3881e5fcecc9
Adarsh Desai <desaiadarsh22@gmail.com>
2023-11-09 08:30:24 +0530

    feat: Expand routes in routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,28 @@ function routes(app: Express) {
   app.get('/healthcheck', (req: Request, res: Response) => {
     res.sendStatus(200);
   });
+
+  // user info routes
+  app.get('/user', (req: Request, res: Response) => {
+    res.sendStatus(200);
+  });
+
+  app.post('/user', (req: Request, res: Response) => {
+    res.sendStatus(200);
+  });
+
+  app.patch('/user', (req: Request, res: Response) => {
+    res.sendStatus(200);
+  });
+
+  app.delete('/user', (req: Request, res: Response) => {
+    res.sendStatus(200);
+  });
+
+  // route to get all documents
+  app.get('/documents', (req: Request, res: Response) => {
+    res.sendStatus(200);
+  });
 }
 
 export default routes;
