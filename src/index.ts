7cdf76db0a750014de2fe071a79ede7b786e5a9e
Adarsh Desai <desaiadarsh22@gmail.com>
2023-12-01 11:33:11 +0530

    refactor: Simplify server start code

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 import { port } from './config/environment';
 import app from './app';
-import { serverStartMessage, serverFailMessage } from './constants';
-import { connectToMongoDB } from './utils/db';
-import log from './utils/logger';
 import routes from './routes';
 
 const start = async () => {
   try {
     app.listen(port, async () => {
-      await connectToMongoDB();
       routes(app);
     });
-    log.info(serverStartMessage + port);
-  } catch {
-    log.info(serverFailMessage);
+  } catch (err) {
+    console.log(err);
   }
 };
 
