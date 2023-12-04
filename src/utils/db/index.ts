f1c2777eaf99da830927dbb63f6877fce99cd705
Adarsh Desai <desaiadarsh22@gmail.com>
2023-12-04 13:38:37 +0530

    feat: Integrate Prisma ORM for PostgreSQL

--- src/utils/db/index.ts
+++ src/utils/db/index.ts
@@ -1,4 +1,5 @@
 import { MongoClient } from 'mongodb';
+import { PrismaClient } from '@prisma/client';
 import { mongoDBUri } from '../../config/environment';
 import { MarkdownContent } from '../../types/database';
 
@@ -7,3 +8,5 @@ export const mongoDBInstance = mongoClient.db();
 
 export const markdownContent =
   mongoDBInstance.collection<MarkdownContent>('markdown-content');
+
+export const primaryDatabase = new PrismaClient();
