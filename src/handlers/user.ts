c460ecffb931306305c6f4bd1c93636e08fd524e
Adarsh Desai <desaiadarsh22@gmail.com>
2023-12-18 14:57:21 +0530

    feat: Enhance user-related handlers in API

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { primaryDatabase } from '../utils/db';
+import { createUserBodySchema } from '../types/requestBodySchema';
+import { ZodError } from 'zod';
 
 export const getUser = async (req: Request, res: Response) => {
   res.sendStatus(200);
@@ -14,7 +16,22 @@ export const getUserById = async (req: Request, res: Response) => {
 };
 
 export const createUser = async (req: Request, res: Response) => {
-  res.sendStatus(200);
+  const { body } = req;
+
+  try {
+    const data = createUserBodySchema.parse(body);
+    try {
+      const user = await primaryDatabase.user.create({
+        data: data
+      });
+      res.status(200).json({ data: user });
+    } catch (e) {
+      res.status(502).json({ error: 'failed database connection' });
+    }
+  } catch (e) {
+    const error = e as ZodError;
+    return res.status(422).json({ errors: error.errors });
+  }
 };
 
 export const updateUser = async (req: Request, res: Response) => {
