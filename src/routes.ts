cad718341847d79de1aa84e9ceab9e8ebbc1bb53
Adarsh Desai <desaiadarsh22@gmail.com>
2024-02-11 20:15:15 +0530

    refactor: streamline user CRUD operations and update middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,10 @@
 import { Express, Request, Response } from 'express';
 import { env } from './config/environment';
-import {
-  getUser,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser
-} from './handlers/user';
+import { getUser, getUserById, handleUserCrud } from './handlers/user';
 
 import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node';
 import { PrismaClient } from '@prisma/client';
+import bodyParser from 'body-parser';
 
 function routes(app: Express) {
   app.get('/user', ClerkExpressRequireAuth(), getUser);
@@ -17,11 +12,11 @@ function routes(app: Express) {
   app.get('/user/:id', ClerkExpressRequireAuth(), getUserById);
 
   // clerk webhook routes, write middlware to deny access to anyone else
-  app.post('/user', createUser);
-
-  app.patch('/user', updateUser);
-
-  app.delete('/user', deleteUser);
+  app.post(
+    '/user',
+    bodyParser.raw({ type: 'application/json' }),
+    handleUserCrud
+  );
 
   // routes to check if the server is repsonding to requests.
   app.get('/servercheck', (req: Request, res: Response) => {
