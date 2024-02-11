ec76f6a24e68580059899ec9ebd40ce314e6bab1
Adarsh Desai <desaiadarsh22@gmail.com>
2024-02-11 20:17:24 +0530

    refactor: transform CRUD handlers to webhook handler

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { primaryDatabase } from '../utils/db';
-import { createUserBodySchema } from '../types/requestBodySchema';
-import { ZodError } from 'zod';
+import { clerkWebhookSigningKey } from '../config/environment';
+import { Webhook } from 'svix';
 
 export const getUser = async (req: Request, res: Response) => {
   res.sendStatus(200);
@@ -15,29 +15,51 @@ export const getUserById = async (req: Request, res: Response) => {
   res.json({ data: user });
 };
 
-export const createUser = async (req: Request, res: Response) => {
-  const { body } = req;
+export const handleUserCrud = async (req: Request, res: Response) => {
+  if (!clerkWebhookSigningKey)
+    throw new Error('you must insert the clerk webhook siging key in your env');
 
-  try {
-    const data = createUserBodySchema.parse(body);
-    try {
-      const user = await primaryDatabase.user.create({
-        data: data
-      });
-      res.status(200).json({ data: user });
-    } catch (e) {
-      res.status(502).json({ error: 'failed database connection' });
-    }
-  } catch (e) {
-    const error = e as ZodError;
-    return res.status(422).json({ errors: error.errors });
+  // grab the headers and the body from the request
+  const headers = req.headers;
+  const payload = req.body;
+
+  // get the svix headers for verification
+  const svixId = headers['svix-id'] as string;
+  const svixTimestamp = headers['svix-timestamp'] as string;
+  const svixSignature = headers['svix-signature'] as string;
+
+  // if there are missing svix headers, error out
+  if (!svixId || !svixTimestamp || !svixSignature) {
+    return new Response('Error: missing svix headers', { status: 400 });
   }
-};
 
-export const updateUser = async (req: Request, res: Response) => {
-  res.sendStatus(200);
-};
+  // initialize svix
+  const wh = new Webhook(clerkWebhookSigningKey);
+  let evt;
 
-export const deleteUser = async (req: Request, res: Response) => {
-  res.sendStatus(200);
+  // attempt to veify the incoming webhook request
+  try {
+    evt = wh.verify(payload, {
+      'svix-id': svixId,
+      'svix-timestamp': svixTimestamp,
+      'svix-signature': svixSignature
+    });
+  } catch (err) {
+    // console log and returen the error
+    console.log('Webhook Verification Failed. Error: ' + err);
+    return res.status(400).json({
+      success: false,
+      message: JSON.stringify(err)
+    });
+  }
+  const id = evt.data;
+  const eventType = evt.type;
+
+  console.log(`Webhook with ID of ${id} and type ${eventType}`);
+  console.log(`Webhook body: ${JSON.stringify(payload)}`);
+
+  return res.status(200).json({
+    success: true,
+    message: 'Webhook Recieved'
+  });
 };
