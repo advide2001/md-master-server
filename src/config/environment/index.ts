06b36c9979ca44efddefa2115cfdd5ce92b49422
Adarsh Desai <desaiadarsh22@gmail.com>
2024-02-14 18:06:12 +0530

    refactor: Simplify environment variables export

--- src/config/environment/index.ts
+++ src/config/environment/index.ts
@@ -11,17 +11,9 @@ const env = {
   production: process.env.NODE_ENV === 'production'
 };
 
-const mongoDBUri = <string>process.env.DATABASE_URL_MONGODB;
 const postgresqlDBUri = <string>process.env.DATABASE_URL_POSTGRESQL;
 const clerkSecretKey = <string>process.env.CLERK_SECRET_KEY;
 
 const clerkWebhookSigningKey = <string>process.env.CLERK_WEBHOOK_SIGNING_KEY;
 
-export {
-  port,
-  env,
-  mongoDBUri,
-  postgresqlDBUri,
-  clerkSecretKey,
-  clerkWebhookSigningKey
-};
+export { port, env, postgresqlDBUri, clerkSecretKey, clerkWebhookSigningKey };
