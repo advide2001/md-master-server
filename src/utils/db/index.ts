9643cc04b8c1b81efeeceb044d3818676667f57c
Adarsh Desai <desaiadarsh22@gmail.com>
2024-02-14 18:08:52 +0530

    refactor: Remove unused MongoDB setup

--- src/utils/db/index.ts
+++ src/utils/db/index.ts
@@ -1,12 +1,3 @@
-import { MongoClient } from 'mongodb';
 import { PrismaClient } from '@prisma/client';
-import { mongoDBUri } from '../../config/environment';
-import { MarkdownContent } from '../../types/database';
-
-export const mongoClient = new MongoClient(mongoDBUri);
-export const mongoDBInstance = mongoClient.db();
-
-export const markdownContent =
-  mongoDBInstance.collection<MarkdownContent>('markdown-content');
 
 export const primaryDatabase = new PrismaClient();
