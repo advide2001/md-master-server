ece7a986d9d124662f6fb977fdcb10463221abb0
Adarsh Desai <desaiadarsh22@gmail.com>
2024-03-20 19:34:09 +0530

    feat: Add type definitions for MarkdownDocument CRUD operations

--- src/utils/db/index.ts
+++ src/utils/db/index.ts
@@ -3,3 +3,5 @@ import { PrismaClient, Prisma } from '@prisma/client';
 export const primaryDatabase = new PrismaClient();
 export type UserCreateInput = Prisma.UserCreateInput;
 export type UserUpdateInput = Prisma.UserUpdateInput;
+export type CreateNewProject = Prisma.MarkdownDocumentCreateInput;
+export type UpdateExistingProject = Prisma.MarkdownDocumentUpdateInput;
