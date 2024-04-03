da5316d2a5b02380b644e01ee1024fff65cc3df9
Adarsh Desai <desaiadarsh22@gmail.com>
2024-04-03 20:09:01 +0530

    refactor: Enhance project handlers with user ID validation

--- src/handlers/projects.ts
+++ src/handlers/projects.ts
@@ -3,12 +3,34 @@ import { Prisma } from '@prisma/client';
 import { CreateNewProject, primaryDatabase } from '../utils/db';
 
 export const getProjects = async (req: Request, res: Response) => {
-  const userId = req?.auth?.claims?.userId as string;
+  const paramUserId = req.query.userId as string;
+  if (paramUserId === undefined || paramUserId === '') {
+    return res.status(400).json({
+      success: false,
+      message: 'Please provide a valid user ID.'
+    });
+  }
+
+  const tokenUserId = req?.auth?.claims?.userId as string;
+  if (tokenUserId === undefined) {
+    return res.status(400).json({
+      success: false,
+      message: 'User ID not found in JWT claims, use the right JWT template!'
+    });
+  }
+
+  if (paramUserId !== tokenUserId) {
+    return res.status(403).json({
+      success: false,
+      message: 'You dont have the authorization to access this resource.'
+    });
+  }
+
   try {
     primaryDatabase.$connect();
     const markdownDocuments = await primaryDatabase.markdownDocument.findMany({
       where: {
-        authorId: userId
+        authorId: paramUserId
       }
     });
 
@@ -35,19 +57,35 @@ export const getProjects = async (req: Request, res: Response) => {
 };
 
 export const createProject = async (req: Request, res: Response) => {
+  const tokenUserId = req?.auth?.claims?.userId as string;
+
+  if (tokenUserId === undefined) {
+    return res.status(400).json({
+      success: false,
+      message: 'User ID not found in JWT claims, use the right JWT template!'
+    });
+  }
+
   const newDocument: CreateNewProject = {
     documentTitle: req.body.title,
     documentContent: req.body.content,
     author: {
       connect: {
-        userId: req?.auth?.claims?.userId as string
+        userId: tokenUserId
       }
     }
   };
   try {
     primaryDatabase.$connect();
     const markdownDocument = await primaryDatabase.markdownDocument.create({
-      data: newDocument
+      data: newDocument,
+      select: {
+        documentId: true,
+        documentTitle: true,
+        createdAt: true,
+        updatedAt: true,
+        authorId: true
+      }
     });
     return res.status(200).json({
       success: true,
@@ -73,22 +111,53 @@ export const createProject = async (req: Request, res: Response) => {
 
 export const updateProject = async (req: Request, res: Response) => {
   const { documentId, newTitle } = req.body;
+  const tokenUserId = req?.auth?.claims?.userId as string;
+
+  if (tokenUserId === undefined) {
+    return res.status(400).json({
+      success: false,
+      message: 'User ID not found in JWT claims, use the right JWT template!'
+    });
+  }
+
+  if (documentId === undefined) {
+    return res.status(400).json({
+      success: false,
+      message: 'Please provide a proper documentId'
+    });
+  }
+
+  if (newTitle === undefined) {
+    return res.status(400).json({
+      success: false,
+      message: 'Please provide a New Title for the document'
+    });
+  }
+
   try {
     primaryDatabase.$connect();
-    const updatedDocument = await primaryDatabase.markdownDocument.update({
+    let markdownDocument = await primaryDatabase.markdownDocument.findUnique({
       where: {
         documentId: documentId
-      },
-      data: {
-        documentTitle: newTitle
       }
     });
 
-    return res.status(200).json({
-      success: true,
-      message: 'Document updated successfully',
-      updatedProject: updatedDocument
-    });
+    if (markdownDocument && markdownDocument.authorId === tokenUserId) {
+      markdownDocument = await primaryDatabase.markdownDocument.update({
+        where: {
+          documentId: documentId
+        },
+        data: {
+          documentTitle: newTitle
+        }
+      });
+
+      return res.status(200).json({
+        success: true,
+        message: 'Document updated successfully',
+        updatedProject: markdownDocument
+      });
+    }
   } catch (err) {
     if (err instanceof Prisma.PrismaClientKnownRequestError) {
       return res.status(400).json({
@@ -107,22 +176,30 @@ export const updateProject = async (req: Request, res: Response) => {
 };
 
 export const deleteProject = async (req: Request, res: Response) => {
-  const projectId = 'clue7lak40000vhnk8unz7vof';
+  const documentId = req.query.documentId as string;
+  const tokenUserId = req?.auth?.claims?.userId as string;
 
   try {
     primaryDatabase.$connect();
-    const deletedMarkdownDocument =
-      await primaryDatabase.markdownDocument.delete({
+    let markdownDocument = await primaryDatabase.markdownDocument.findUnique({
+      where: {
+        documentId: documentId
+      }
+    });
+
+    if (markdownDocument && markdownDocument.authorId === tokenUserId) {
+      markdownDocument = await primaryDatabase.markdownDocument.delete({
         where: {
-          documentId: projectId
+          documentId: documentId
         }
       });
 
-    return res.status(200).json({
-      success: true,
-      message: 'document deleted successfully',
-      deletedProject: deletedMarkdownDocument
-    });
+      return res.status(200).json({
+        success: true,
+        message: 'document deleted successfully',
+        deletedProject: markdownDocument
+      });
+    }
   } catch (err) {
     if (err instanceof Prisma.PrismaClientKnownRequestError) {
       return res.status(400).json({
